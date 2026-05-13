cb3bcbe3e49d999450561bdace1390cd19503b71
yehlin <yehlin@mit.edu>
2026-05-13 01:00:19 -0400

    clean up SaProtdG_dms.py: remove frag sequences and unused imports

--- scripts/SaProtdG_dms.py
+++ scripts/SaProtdG_dms.py
@@ -9,59 +9,17 @@ _ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, _ROOT)
 
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.utils.data import DataLoader
-
-import pytorch_lightning as pl
-from pytorch_lightning.callbacks import ModelCheckpoint
-from pytorch_lightning.loggers import WandbLogger
-from torchmetrics import MeanSquaredError, R2Score, SpearmanCorrCoef, PearsonCorrCoef
-from omegaconf import OmegaConf
-
-from utils.foldseek_util import get_struc_seq
-import torch
-from torch.utils.data import ConcatDataset
 import pandas as pd
 import numpy as np
-import pickle
-
-from Bio import pairwise2
-from math import isnan
-from tqdm import tqdm
-from dataclasses import dataclass
-from typing import Optional
-from torch.utils.data import ConcatDataset
-import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
-
-ALPHABET = 'ACDEFGHIKLMNPQRSTVWY-'
-from utils.config import get_default_config, get_model_configs
 from SaProtdG import SaProtdG, SaProtdG_predict
 
 import logging
 logging.getLogger("pytorch_lightning").setLevel(logging.ERROR)
 
-
-def get_pdb(pdb_code=""):
-  if pdb_code is None or pdb_code == "":
-    upload_dict = files.upload()
-    pdb_string = upload_dict[list(upload_dict.keys())[0]]
-    with open("tmp.pdb","wb") as out: out.write(pdb_string)
-    return "tmp.pdb"
-  elif os.path.isfile(pdb_code):
-    return pdb_code
-  elif len(pdb_code) == 4:
-    os.system(f"wget -qnc https://files.rcsb.org/view/{pdb_code}.pdb")
-    return f"{pdb_code}.pdb"
-  else:
-    os.system(f"wget -qnc https://alphafold.ebi.ac.uk/files/AF-{pdb_code}-F1-model_v3.pdb")
-    return f"AF-{pdb_code}-F1-model_v3.pdb"
-  
-cfg = get_default_config()
-ddg_scanning = True 
-cfg.testing.ddg_scanning = ddg_scanning
+ALPHABET = 'ACDEFGHIKLMNPQRSTVWY-'
 
 PDB_NAME = 'nanobody_1zvh'
 CHAIN_ID = 'A'
@@ -69,86 +27,50 @@ WEIGHTS_DIR = os.path.join(_ROOT, "saprotdg_weights")
 WEIGHT_FILES = [
     "SaProtdG_weights_1_lora.ckpt",
     "SaProtdG_weights_2_lora.ckpt",
-    "SaProtdG_weights_3_lora.ckpt"
-]  
+    "SaProtdG_weights_3_lora.ckpt",
+]
+
 pdb_path = os.path.join(_ROOT, "examples", "nanobody_1zvh.cif")
-frag1='M#E#D#'
-frag2='AdKpNqIkKfKaGaPdAaPfFpYdPhLqEdDdGfTfAlGlEqQlLlHlKvAlMlKvRvYqAlLvVpPpGpTlIfAlFeTaDaAlHvIvEgVdNtIdTgYsAvEnYlFlElMlSlVlRlLlAlEqAlMvKvRqYvGpLdNaTlNvHfReIeVeVeCaSeEfNdSdLpQqFrFcMsPnVlLsGsAcLsFlIrGlVhAeVyAePyAqNyDnIvYqNaElRvEcLvLlNlSsMcNvIlSrQlPhTqVeVyFeVySaKvKvGcLvQvKsIvLvNvVsQcKvKvLrPvIsIhQpKaIyIeIhMrDpSdKcTqDaYdQpGnFhQhShMsYnTvFsVsTvSvHgLdPdPpGpFdNdEsYvDvFrVhPgEdStFdDpRqDaKpTgIfAsLyIqMaNkSp'
-frag3='S#G#S#T#G#'
-frag4='LvPtKfGtVfAtLwPgHsRqTlAlCsVvRlFlSsHqAcRcDgPvIwFnGhNnQpIlIdPpDlTaAeIeLeSeVaVdPgFcHsHhGlFqGnMvFnTnTsLsGnYvLsIsCsGsFyRyVyVyLyMyYrRdFdEdEvEvLsFvLlRvScLlQaDvYsKlIhQqSeAyLeLdVaPqTvLvFlSvFvFqAlKpSdTpLcInDvKvYgDdLnSvNnLhHaEaIyAeShGkGqAdPfLySaKpEvVsGqEvAsVnAcKvRsFnHvLyPpGtIyRkQaGaYhGdLgTvEqTlTsStAgIqLaIiTaPtEpGp'
-frag5='D#D#K#'
-frag6='PrGrAqVrGhKaVgVhPhFqFkEiAkKfVfVaDaLqDpTpGrKhTtLdGhVaNpQdRkGhEfLiCkVmRaGgPrMsIgMtSpGaYtVrNpNpPvEpAvTrNpAvLqIaDdKpDvGrWiLgHgSpGlDwIiAwYgWaDhEpDvErHgFiFdItVd'
-frag7='D#R#L#K#S#'
-frag8='LfIfKqYfKqGnYdQtVdAdPqAvEvLlEqSvIlLqLcQpHdPqNqIfFpDgAkGgVwAeGfLqPaDdDvDvArGrEtLaPiAeAmVeVtVaLtEdHpGpKhTdMdTdEqKvErIsVqDvYsVsApSvQpVdTdTvArKhKgLrRpGlGtVyVhFyVdDnEdVqPc'
-frag9='K#G#L#T#G#'
-frag10='KvLhDsArRvKvIrRnEvIvLsInKvAvKrKd'
-frag11='G#G#K#S#K#L#'
-# combined_seq_1lci = frag1 + frag2 + frag3 + frag4 + frag5 + frag6 + frag7 + frag8 + frag9 + frag10 + frag11
-combined_seq_1lci = frag1 + frag2 + frag3 + frag4 + frag5 + frag6
 
 all_mean_results = []
 for i, weight_file in enumerate(WEIGHT_FILES, 1):
     weight_path = f"{WEIGHTS_DIR}/{weight_file}"
-    model = SaProtdG(weight_path, cfg)
-    pred_mutant_ddg, scaled_pred_mutant_ddg, combined_seq = SaProtdG_predict(model, pdb_path, CHAIN_ID, ddg_scanning, given_seq=combined_seq_1lci)
+    model = SaProtdG(weight_path)
+    pred_mutant_ddg, scaled_pred_mutant_ddg, combined_seq = SaProtdG_predict(model, pdb_path, CHAIN_ID, ddg_scanning=True)
     mean_results = np.mean(scaled_pred_mutant_ddg.detach().cpu().numpy(), axis=-1).T[0]
     all_mean_results.append(mean_results)
-    
-    # Save individual model results
+
     df = pd.DataFrame(mean_results, index=list(ALPHABET[:-1]), columns=range(1, mean_results.shape[1] + 1))
-    df.to_csv(f'mutational_scanning_{PDB_NAME}_model{i}_truncated.csv')
-    
-    # Plot individual model results
+    df.to_csv(f'mutational_scanning_{PDB_NAME}_model{i}.csv')
+
     height, width = mean_results.shape
-    fig_width = min(max(width/4, 8), 25)
-    fig_height = min(max(height/4, 3), 8)
+    fig_width = min(max(width / 4, 8), 25)
+    fig_height = min(max(height / 4, 3), 8)
     plt.figure(figsize=(fig_width, fig_height))
-    
-    vmin = mean_results.min()
-    vmax = mean_results.max()
-    divergence = max(abs(vmin), abs(vmax))
-    
-    plt.imshow(mean_results, 
-               aspect='auto',
-               cmap='bwr',
-               vmin=-divergence,
-               vmax=divergence)
-    
-    L = mean_results.shape[1]
-    plt.xticks(ticks=np.arange(L), labels=np.arange(1, L + 1), rotation=90)
-    plt.yticks(ticks=range(len(ALPHABET)-1), labels=ALPHABET[:-1])
+    divergence = max(abs(mean_results.min()), abs(mean_results.max()))
+    plt.imshow(mean_results, aspect='auto', cmap='bwr', vmin=-divergence, vmax=divergence)
+    plt.xticks(ticks=np.arange(width), labels=np.arange(1, width + 1), rotation=90)
+    plt.yticks(ticks=range(len(ALPHABET) - 1), labels=ALPHABET[:-1])
     plt.ylabel("Amino Acids")
     plt.title(f"Mutational Scanning {PDB_NAME} - Model {i}")
     plt.colorbar()
-    plt.savefig(f'mutational_scanning_{PDB_NAME}_model{i}_truncated.png', dpi=300, bbox_inches='tight')
+    plt.savefig(f'mutational_scanning_{PDB_NAME}_model{i}.png', dpi=300, bbox_inches='tight')
     plt.show()
 
-# Average results across models
+# Ensemble
 mean_results = np.mean(all_mean_results, axis=0)
 height, width = mean_results.shape
 
-# Save ensemble results
 df = pd.DataFrame(mean_results, index=list(ALPHABET[:-1]), columns=range(1, width + 1))
-df.to_csv(f'mutational_scanning_{PDB_NAME}_ensemble_truncated.csv')
+df.to_csv(f'mutational_scanning_{PDB_NAME}_ensemble.csv')
 
-# Plot ensemble results
-fig_width = min(max(width/4, 8), 25)
-fig_height = min(max(height/4, 3), 8)
+fig_width = min(max(width / 4, 8), 25)
+fig_height = min(max(height / 4, 3), 8)
 plt.figure(figsize=(fig_width, fig_height))
-
-vmin = mean_results.min()
-vmax = mean_results.max()
-divergence = max(abs(vmin), abs(vmax))
-
-plt.imshow(mean_results, 
-           aspect='auto',
-           cmap='bwr',
-           vmin=-divergence,
-           vmax=divergence)
-
-L = mean_results.shape[1]
-plt.xticks(ticks=np.arange(L), labels=np.arange(1, L + 1), rotation=90)
-plt.yticks(ticks=range(len(ALPHABET)-1), labels=ALPHABET[:-1])
+divergence = max(abs(mean_results.min()), abs(mean_results.max()))
+plt.imshow(mean_results, aspect='auto', cmap='bwr', vmin=-divergence, vmax=divergence)
+plt.xticks(ticks=np.arange(width), labels=np.arange(1, width + 1), rotation=90)
+plt.yticks(ticks=range(len(ALPHABET) - 1), labels=ALPHABET[:-1])
 plt.ylabel("Amino Acids")
 plt.title(f"Mutational Scanning {PDB_NAME} - Ensemble")
 plt.colorbar()
